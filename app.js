02599a76dadcd79ccbb8829b1c337c2bfcf8c53c
Ally Juma Said <mahadsaid95@icloud.com>
2023-06-14 23:54:42 +0100

    adding

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const app = Vue.createApp({
  template : '',
   data(){
     return {
-        firstName : 'Mahad',
-        lastName : "Said",
+        firstName : 'Wayab',
+        lastName : "Meow",
         email : "mahad@exampleemail.com",
         gender : "male",
         picture : "https://randomuser.me/api/portraits/men/10.jpg"
